444080392737a8ab837deee84c3de250d3437976
den <denvstheworld@gmail.com>
2012-10-03 21:59:38 -0400

    Created Java Properties version of config system. Created Forge Properties version of config system.

--- denoflionsx/denLib/Config/Config_JavaProperties.java
+++ denoflionsx/denLib/Config/Config_JavaProperties.java
@@ -3,20 +3,30 @@ package denoflionsx.denLib.Config;
 import java.io.File;
 import java.util.ArrayList;
 import denoflionsx.denLib.denLib;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Properties;
 
-public class Config {
+public class Config_JavaProperties {
 
     public static String ConfigDir;
     protected String ConfigFile = "";
     public static boolean verbose = false;
     public static final boolean debug = false;
-    public ForgeConfigExtended Options;
-    public ForgeConfigExtended Defaults;
+    public static MODE mode = MODE.INI;
+    public Properties Options;
+    public Properties Defaults;
 
-    public Config(String c) {
+    public Config_JavaProperties(String c) {
+        if (mode.equals(MODE.XML)){
+            String[] split = c.split(".");
+            for (String s : split){
+                denLib.print(s);
+            }
+        }
         this.ConfigFile = ConfigDir + c;
-        Options = new ForgeConfigExtended(new File(this.ConfigFile));
-        Defaults = new ForgeConfigExtended(new File(this.ConfigFile));
+        Options = new Properties();
+        Defaults = new Properties();
     }
 
     public String getConfigFile() {
@@ -29,9 +39,11 @@ public class Config {
 
     public void readFile() {
         try {
-
-            Options.load();
-
+            if (mode.equals(MODE.INI)) {
+                Options.load(new FileInputStream(this.ConfigFile));
+            }else if (mode.equals(MODE.XML)){
+                Options.loadFromXML(new FileInputStream(this.ConfigFile));
+            }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
@@ -63,33 +75,40 @@ public class Config {
     }
 
     public void setOption(String key, String param) {
+        Options.put(key, param);
     }
 
     public String getOption(String key) {
         String op = "";
-        if (Options.getOption(key) == null) {
-            for (String s : Defaults.dumpAllKeys()) {
+        if (Options.get(key) == null) {
+            for (String s : denLib.dumpPropertiesKeys(Defaults)) {
                 if (s.equals(key)) {
-                    Options.addDefault(key + "=" + Defaults.getOption(key));
+                    Options.setProperty(key, Defaults.getProperty(key));
                 }
             }
         }
-        if (Options.getOption(key) != null) {
-            op = Options.getOption(key).toString();
+        if (Options.get(key) != null) {
+            op = Options.get(key).toString();
             //denLib.print(key + " = " + op);
         }
         return op;
     }
 
     public String getOptionNoUpdate(String key) {
-        if (Options.getOption(key) == null) {
+        if (Options.get(key) == null) {
             return null;
         }
-        return Options.getOption(key).toString();
+        return Options.get(key).toString();
     }
 
     public void addDefault(String d) {
-        Defaults.addDefault(d);
+        if (d.contains("#") || (d.contains("[") && d.contains("]"))) {
+            return;
+        }
+        String[] split = d.split("=");
+        String key = split[0];
+        String value = split[1];
+        Defaults.setProperty(key, value);
     }
 
     public boolean doesConfigExist() {
@@ -99,10 +118,11 @@ public class Config {
     public void writeConfig() {
         try {
             if (!doesConfigExist()) {
-                Defaults.save();
-            }else{
-                this.deleteConfig();
-                Options.save();
+                if (mode.equals(MODE.INI)) {
+                    Defaults.store(new FileOutputStream(this.ConfigFile), null);
+                } else if (mode.equals(MODE.XML)) {
+                    Defaults.storeToXML(new FileOutputStream(this.ConfigFile), null);
+                }
             }
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -118,10 +138,21 @@ public class Config {
     public ArrayList<String> dumpValues() {
         // This dumps all values without the keys.
         // Good for uses where the key is arbitrary.
-        return Options.dumpAllValues();
+        ArrayList<String> values = new ArrayList();
+        for (Object o : Options.values()) {
+            String v = o.toString();
+            values.add(v);
+        }
+        return values;
     }
 
     public ArrayList<String> dumpKeys() {
-        return Options.dumpAllKeys();
+        return denLib.dumpPropertiesKeys(Options);
+    }
+
+    public enum MODE {
+
+        INI,
+        XML;
     }
 }
