4355e872c612ccdd6ee7203c3b3324cb6abce5ef
den <denvstheworld@gmail.com>
2013-05-29 19:20:17 -0400

    Fixed GateCopy name issue.

--- src/denoflionsx/denLib/Mod/Items/ItemMeta.java
+++ src/denoflionsx/denLib/Mod/Items/ItemMeta.java
@@ -15,28 +15,28 @@ import net.minecraft.nbt.NBTTagString;
 import net.minecraft.util.Icon;
 
 public class ItemMeta extends Item {
-
+    
     protected ArrayList<ItemStack> stacks = new ArrayList();
     public HashMap<Integer, Icon> icons = new HashMap();
     protected HashMap<Integer, String> names = new HashMap();
     protected ArrayList<String> textures = new ArrayList();
-
+    
     public ItemMeta(int par1) {
         super(par1);
     }
-
+    
     public ItemMeta(String[] textures, int par1) {
         super(par1);
         this.textures.addAll(Arrays.asList(textures));
     }
-
+    
     public ItemStack createItemEntry(int meta, String name) {
         ItemStack i = new ItemStack(this, 1, meta);
         stacks.add(i);
         names.put(meta, name);
         return i;
     }
-
+    
     public ItemStack createItemEntry(int meta, String name, String texture) {
         ItemStack i = new ItemStack(this, 1, meta);
         stacks.add(i);
@@ -44,7 +44,7 @@ public class ItemMeta extends Item {
         textures.add(meta, texture);
         return i;
     }
-
+    
     public ItemStack createItemEntry(int meta, String name, NBTTagCompound tag) {
         ItemStack i = new ItemStack(this, 1, meta);
         i.stackTagCompound = tag;
@@ -52,12 +52,12 @@ public class ItemMeta extends Item {
         names.put(meta, name);
         return i;
     }
-
+    
     @Override
     public void getSubItems(int par1, CreativeTabs par2CreativeTabs, List par3List) {
         par3List.addAll(stacks);
     }
-
+    
     @Override
     public void addInformation(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, List par3List, boolean par4) {
         if (par1ItemStack.stackTagCompound != null) {
@@ -70,14 +70,14 @@ public class ItemMeta extends Item {
             }
         }
     }
-
+    
     @Override
     public void registerIcons(IconRegister par1IconRegister) {
         for (String s : textures) {
             this.icons.put(textures.indexOf(s), par1IconRegister.registerIcon(s));
         }
     }
-
+    
     @Override
     public Icon getIconFromDamage(int par1) {
         if (icons.get(par1) != null) {
@@ -86,7 +86,7 @@ public class ItemMeta extends Item {
             return icons.get(0);
         }
     }
-
+    
     @Override
     public String getItemDisplayName(ItemStack par1ItemStack) {
         if (par1ItemStack.stackTagCompound != null) {
@@ -94,20 +94,14 @@ public class ItemMeta extends Item {
                 NBTTagCompound tag = par1ItemStack.stackTagCompound.getCompoundTag("override");
                 return tag.getString("name");
             }
-        } else {
-            if (this.names.get(par1ItemStack.getItemDamage()) != null) {
-                return this.names.get(par1ItemStack.getItemDamage());
-            } else {
-                return this.names.get(0);
-            }
         }
-        return denLib.StringUtils.readError;
+        return this.names.get(par1ItemStack.getItemDamage());
     }
-
+    
     public String getBaseName() {
         return names.get(0);
     }
-
+    
     @Override
     public boolean getHasSubtypes() {
         return true;
