a51a53f815b219618681a0538556069e19305aca
denoflionsx <denvstheworld@gmail.com>
2013-11-13 20:13:00 -0500

    Add constructor for incoming packets.

--- src/denoflionsx/denLib/Mod/Net/Packet/DenPacket.java
+++ src/denoflionsx/denLib/Mod/Net/Packet/DenPacket.java
@@ -5,17 +5,27 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.packet.Packet250CustomPayload;
 
 public class DenPacket extends Packet250CustomPayload {
-    
+
+    private static final String id = "id";
+
     public DenPacket(int packetID, NBTTagCompound payload) {
         try {
             NBTTagCompound t = (NBTTagCompound) payload.copy();
-            t.setInteger("id", packetID);
+            t.setInteger(id, packetID);
             this.data = CompressedStreamTools.compress(t);
+            this.length = data.length;
         } catch (Throwable t) {
             t.printStackTrace();
         }
     }
-    
+
+    public DenPacket(Packet250CustomPayload packet) {
+        this.data = packet.data;
+        this.channel = packet.channel;
+        this.isChunkDataPacket = packet.isChunkDataPacket;
+        this.length = packet.length;
+    }
+
     public NBTTagCompound getPayload() {
         try {
             return CompressedStreamTools.decompress(this.data);
@@ -24,5 +34,9 @@ public class DenPacket extends Packet250CustomPayload {
         }
         return new NBTTagCompound();
     }
-    
+
+    public int getPacketID() {
+        return getPayload().getInteger(id);
+    }
+
 }
